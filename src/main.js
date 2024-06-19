54c87f141908c137460a31890893469eaf20b4f8
bradfish29 <duongkhai2904@gmail.com>
2024-06-19 23:22:36 +0700

    Change: update from mdbootstrap-vue to mdbootstrap-plain-js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
+import min_css from "./assets/css/mdb.min.css";
 
-createApp(App).use(router).mount("#app");
+
+createApp(App).use(router).use(min_css).mount("#app");
