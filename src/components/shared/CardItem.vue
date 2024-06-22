237e1bf18a66fbd49675fc18639ef7c57d78a54d
Wolrine <volam030102@gmail.com>
2024-06-22 08:08:18 +0700

    Add home page and car item

---
+++ src/components/shared/CardItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- <div id="card-item" class="container-fluid row">
+    </div> -->
+    <div class="card mb-3 m-3 left-50" style="max-width: 520px; background: #F1E6D6;">
+        <div class="row g-0">
+            <div class="col-md-4 container d-flex align-items-center flex-column mt-3">
+                <img src="https://s3-alpha-sig.figma.com/img/1697/31c5/2b682e674e48b548f107448263f6c265?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gr4tvVLlfGH9VZtX18PZE5oeEGX23cXW~v-g-ayG0BYrd82iO3D6zeAnTM6gViC6wKqNRUnBoH51J3wxKvisyX2Gvh5RLl2xFKiTPoMCugandTXENez-bBJJZxC1hywpFGHG01nvhPzvGKoHMcJmdmAA~kOUPz~Nsy7p5apuWX8RA4~K17ut4KN2g9Pdc7gWtAWrAtOm2U3u4SPxqR9wr6WG84A~Z7n8pDTN5pm~~06d~xEP48lzKDOTDAcvyyJFLaE4HWj3CerCSvFetE84oj9qWdlzgPTta~7oxeWPa0pO3cN2sNbqjtpVM4sGbRdHB0zAJ3KJ~Ko7GWyM-pWhHQ__"
+                    alt="Trendy Pants and Shoes" class="img-fluid rounded-start" />
+                <button class="btn-block" style="background-color: #D2E3C8;"><i class="fas fa-face-laugh"></i> Oda</button>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title mt-auto p-2">One Piece - Đảo hải tặc</h5>
+                    <div class="mt-auto d-flex flex-column-reverse p-2">
+                        <button class="btn my-bg-primary text-light p-2 m-2">Chap 1241</button>
+                        <button class="btn my-bg-primary text-light p-2 m-2">Chap 1242</button>
+                        <button class="btn my-bg-primary text-light p-2 m-2">Chap 1243</button>
+                    </div>
+                    <p class="card-text mt-auto p-2">
+                        <small class="text-muted"><i class="fas fa-clock-rotate-left"></i> 13 giờ trước</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
